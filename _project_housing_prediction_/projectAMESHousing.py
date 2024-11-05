1810130006319e9c86d5b62f09731b8ae0cfa99d
Vijay Subedi <70833993+vijaybsubedi@users.noreply.github.com>
2024-11-05 18:53:43 -0500

    initial phase committed

---
+++ _project_housing_prediction_/projectAMESHousing.py
@@ -0,0 +1,8 @@
+# import all required dependencies
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Creating a class called HousingPrediction Model
+# Based on Linear Regression Model (Ordinary Least Squares) 
